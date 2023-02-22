059c60ee66e306d02a4dc0fc1502b574db59f8c3
sucongohu <1126045770@qq.com>
2023-02-22 15:00:07 +0800

    format

--- src/main.nim
+++ src/main.nim
@@ -1,21 +1,21 @@
-import ws,json,strutils,parseopt, asyncdispatch, asynchttpserver
+import ws, json, strutils, parseopt, asyncdispatch, asynchttpserver
 
-type 
+type
   User = ref object
     ws*: Websocket
-    id*: string 
+    id*: string
 
 
 type Config* = object
-    port*: uint
+  port*: uint
 
 
 var connections = newSeq[User]()
 
-proc range(fn:proc(u:User):bool) =
+proc range(fn: proc(u: User): bool) =
   var alive = newSeq[User]()
   var change = false
-  for i,user in connections:
+  for i, user in connections:
     let t = fn(user)
     if t:
       alive.add(user)
@@ -23,9 +23,9 @@ proc range(fn:proc(u:User):bool) =
       change = true
   if change:
     connections = alive
-  
 
-proc isOnLine(u :User):bool  = 
+
+proc isOnLine(u: User): bool =
   if u.ws.readyState != Open:
     try:
       u.ws.close()
@@ -41,8 +41,8 @@ proc isOnLine(u :User):bool  =
   return true
 
 
-proc broadcastIf(text:string,fn:proc(id:string):bool ) {.async} = 
-  proc each(u:User): bool  =
+proc broadcastIf(text: string, fn: proc(id: string): bool) {.async.} =
+  proc each(u: User): bool =
     if u.ws.readyState != Open:
       return false
     let ok = fn(u.id)
@@ -60,8 +60,8 @@ proc broadcastIf(text:string,fn:proc(id:string):bool ) {.async} =
   range(each)
 
 
-proc getInitData():string = 
-  var ids =  newSeq[string]()
+proc getInitData(): string =
+  var ids = newSeq[string]()
   for user in connections:
     if user.id notin ids:
       ids.add(user.id)
@@ -72,16 +72,16 @@ proc getInitData():string =
   return $data
 
 
-proc getOnlineData(id:string):string = 
+proc getOnlineData(id: string): string =
   let data = %*{
-    "event":"online",
-    "id":id,
+    "event": "online",
+    "id": id,
   }
   return $data
 
-proc handle(req:Request,id:string) {.async, gcsafe.} = 
+proc handle(req: Request, id: string) {.async, gcsafe.} =
   let ws = await newWebSocket(req)
-  let r= ws.send(getInitData())
+  let r = ws.send(getInitData())
   yield r
   if r.failed:
     try:
@@ -90,53 +90,53 @@ proc handle(req:Request,id:string) {.async, gcsafe.} =
       discard
     finally:
       return
-  proc others(tid:string):bool = 
-    return tid!=id
-  await broadcastIf(getOnlineData(id),others)
-  connections.add(User(ws:ws,id:id))
-  var packet:string
-  var data :JsonNode
+  proc others(tid: string): bool =
+    return tid != id
+  await broadcastIf(getOnlineData(id), others)
+  connections.add(User(ws: ws, id: id))
+  var packet: string
+  var data: JsonNode
   while ws.readyState == Open:
     try:
       packet = await ws.receiveStrPacket()
     except Exception:
       break
     try:
-      data= parseJson(packet)
+      data = parseJson(packet)
     except:
       continue
     if data.kind == JObject and data.hasKey("to"):
       let to = data["to"].getStr()
-      if to!="":
-        proc touser(tid:string):bool=
-          return tid==to
-        await broadcastIf(packet,touser)
+      if to != "":
+        proc touser(tid: string): bool =
+          return tid == to
+        await broadcastIf(packet, touser)
   range(isOnLine)
 
 proc getConfig(): Config =
-  var cfg = Config( port: 9001,)
+  var cfg = Config(port: 9001, )
   let err = "invalid argument"
   for kind, key, val in getopt():
-      case kind
-      of cmdArgument:
-          assert(false,err);
-      of cmdLongOption, cmdShortOption:
-          case key
-          of "port", "p": cfg.port = parseUint(val)
-      of cmdEnd: assert(false,err)
+    case kind
+    of cmdArgument:
+      assert(false, err);
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "port", "p": cfg.port = parseUint(val)
+    of cmdEnd: assert(false, err)
   return cfg
-  
+
 
 proc cb(req: Request) {.async, gcsafe.} =
   try:
     let arr = req.url.path.split("/uid/")
-    if len(arr)==2:
-        if len(arr[1])==36:
-          await handle(req,arr[1])
-          return
-        elif arr[1] == "status" :
-          await req.respond(Http200, getInitData())
-          return
+    if len(arr) == 2:
+      if len(arr[1]) == 36:
+        await handle(req, arr[1])
+        return
+      elif arr[1] == "status":
+        await req.respond(Http200, getInitData())
+        return
     await req.respond(Http200, "Hello World")
   except Exception:
     echo "error:", getCurrentExceptionMsg()
