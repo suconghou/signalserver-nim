fa44330bddda1239f380954021104b05466a83a2
suconghou <1126045770@qq.com>
2024-08-05 16:52:54 +0800

    update ws async loop

--- src/main.nim
+++ src/main.nim
@@ -1,147 +1,120 @@
-import ws, json, strutils, parseopt, asyncdispatch, asynchttpserver
+import ws, json, tables, sequtils, strutils, parseopt, asyncdispatch, asynchttpserver
+
 
 type
-  User = ref object
-    ws*: Websocket
-    id*: string
+    User = ref object
+        ws*: Websocket
+        uid*: string
+        id*: uint
 
 
 type Config* = object
-  port*: uint
+    port*: uint
+
+var uqid {.threadvar.}: uint
+var connections {.threadvar.}: TableRef[uint, User]
 
 
-var connections {.threadvar.}: seq[User]
-
-proc range(fn: proc(u: User): bool) =
-  var alive = newSeq[User]()
-  var change = false
-  for i, user in connections:
-    let t = fn(user)
-    if t:
-      alive.add(user)
-    else:
-      change = true
-  if change:
-    connections = alive
-
-
-proc isOnLine(u: User): bool =
-  if u.ws.readyState != Open:
+proc hangup(user: User) =
     try:
-      u.ws.close()
-    except CatchableError:
-      discard
-    return false
-  proc check() {.async.} =
+        user.ws.hangup()
+    except Exception:
+        return
+
+
+proc broadcast(text: string, fn: proc(user: User): bool {.noSideEffect.}) {.async.} =
+    for user in connections.values.toSeq():
+        if fn(user):
+            try:
+                await user.ws.send(text)
+            except Exception:
+                user.hangup()
+                connections.del(user.id)
+
+proc users(): string =
+    let ids = newTable[string, bool]()
+    for user in connections.values:
+        ids[user.uid] = true
+    let data = %*{
+          "event": "init",
+          "ids": ids.keys.toSeq(),
+    }
+    return $data
+
+
+func notify(uid: string): string =
+    let data = %*{
+      "event": "online",
+      "id": uid,
+    }
+    return $data
+
+proc handle(req: Request, uid: string): Future[void] {.async.} =
+    inc(uqid)
+    let user = User(ws: await newWebSocket(req), uid: uid, id: uqid)
     try:
-      await u.ws.ping()
-    except CatchableError:
-      u.ws.close()
-  asyncCheck check()
-  return true
+        await user.ws.send(users())
+    except Exception:
+        user.hangup()
+        return
+    connections[user.id] = user
 
-
-proc broadcastIf(text: string, fn: proc(id: string): bool {.noSideEffect.}) {.async.} =
-  proc each(u: User): bool =
-    if u.ws.readyState != Open:
-      return false
-    let ok = fn(u.id)
-    if ok:
-      try:
-        asyncCheck u.ws.send(text)
-      except CatchableError:
+    func others(u: User): bool =
+        return u.id != user.id
+    await broadcast(notify(uid), others)
+    var packet: string
+    var data: JsonNode
+    while user.ws.readyState == Open:
         try:
-          u.ws.close()
-        except CatchableError:
-          discard
-        finally:
-          return false
-    return true
-  range(each)
-
-
-proc getInitData(): string =
-  var ids = newSeq[string]()
-  for user in connections:
-    if user.id notin ids:
-      ids.add(user.id)
-  let data = %*{
-        "event": "init",
-        "ids": ids,
-  }
-  return $data
-
-
-func getOnlineData(id: string): string =
-  let data = %*{
-    "event": "online",
-    "id": id,
-  }
-  return $data
-
-proc handle(req: Request, id: string) {.async.} =
-  let ws = await newWebSocket(req)
-  let r = ws.send(getInitData())
-  yield r
-  if r.failed:
-    try:
-      ws.close()
-    except CatchableError:
-      discard
-    finally:
-      return
-  func others(tid: string): bool =
-    return tid != id
-  await broadcastIf(getOnlineData(id), others)
-  connections.add(User(ws: ws, id: id))
-  var packet: string
-  var data: JsonNode
-  while ws.readyState == Open:
-    try:
-      packet = await ws.receiveStrPacket()
-    except CatchableError:
-      break
-    try:
-      data = parseJson(packet)
-    except CatchableError:
-      continue
-    if data.kind == JObject and data.hasKey("to"):
-      let to = data["to"].getStr()
-      if to != "":
-        func touser(tid: string): bool =
-          return tid == to
-        await broadcastIf(packet, touser)
-  range(isOnLine)
+            packet = await user.ws.receiveStrPacket()
+        except Exception:
+            break
+        try:
+            data = parseJson(packet)
+        except Exception:
+            continue
+        if data.kind == JObject and data.hasKey("to"):
+            let to = data["to"].getStr()
+            if to != "":
+                func touser(u: User): bool =
+                    return u.uid == to
+                await broadcast(packet, touser)
+    user.ws.readyState = Closed
+    for user in connections.values.toSeq():
+        if user.ws.readyState != Open:
+            user.hangup()
+            connections.del(user.id)
 
 proc getConfig(): Config =
-  var cfg = Config(port: 9001, )
-  let err = "invalid argument"
-  for kind, key, val in getopt():
-    case kind
-    of cmdArgument:
-      assert(false, err);
-    of cmdLongOption, cmdShortOption:
-      case key
-      of "port", "p": cfg.port = parseUint(val)
-    of cmdEnd: assert(false, err)
-  return cfg
+    var cfg = Config(port: 9001, )
+    let err = "invalid argument"
+    for kind, key, val in getopt():
+        case kind
+        of cmdArgument:
+            assert(false, err);
+        of cmdLongOption, cmdShortOption:
+            case key
+            of "port", "p": cfg.port = parseUint(val)
+        of cmdEnd: assert(false, err)
+    return cfg
 
 
 proc cb(req: Request) {.async, gcsafe.} =
-  try:
-    let arr = req.url.path.split("/uid/")
-    if len(arr) == 2:
-      if len(arr[1]) == 36:
-        await handle(req, arr[1])
-        return
-      elif arr[1] == "status":
-        await req.respond(Http200, getInitData())
-        return
-    await req.respond(Http200, "Hello World")
-  except CatchableError:
-    echo "error:", getCurrentExceptionMsg()
+    try:
+        let arr = req.url.path.split("/uid/")
+        if len(arr) == 2:
+            if len(arr[1]) == 36:
+                await handle(req, arr[1])
+                return
+            elif arr[1] == "status":
+                await req.respond(Http200, users())
+                return
+        await req.respond(Http200, "Hello World")
+    except Exception:
+        echo "error:", getCurrentExceptionMsg()
 
-connections = newSeq[User]()
+uqid = 0
+connections = newTable[uint, User]()
 let cfg = getConfig()
 let server = newAsyncHttpServer()
 waitFor server.serve(Port(cfg.port), cb)
